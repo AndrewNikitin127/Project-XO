d40ef3074a47c979f23ff4d80d4c9222064e00a7
MrNovan <toni.justdoit@bk.ru>
2023-06-10 16:00:12 +0300

    add avatars

--- src/avatars.js
+++ src/avatars.js
@@ -1,7 +1,12 @@
 import readlineSync from 'readline-sync';
 
-const avatarCat = ['  (\\__/)\n  (=‘.’=)\nE[:]|||||[:]З\n  (“)_(”)',
-  "`(“).(”)\n`(’ o ') \n (“) `(”)\n(“”)-(“”)"];
+const avatarCat = ['  (\\___/)\n   (=‘.’=)\nE[:]|||||[:]З\n   (“)_(”)',
+  '/\\_/\\\n((@v@))\n():::()\n VV-VV',
+  '/\\_/\\\n( o.o )\n > ^ <',
+  '((...))\n ( o o )\n  \\   /\n   ^_^',
+  '_   _\n(_)_(_)\n (o o)\n==\\o/==',
+  '((_,...,_))\n    |o o|\n    \\   /\n     ^_^',
+  '__    __\n/ \\\\..// \\\n  ( oo )\n   \\__/'];
 
 const selectAvatar = () => {
   const indexAvatar = readlineSync.keyInSelect(avatarCat, 'Выбери аватарку:');
