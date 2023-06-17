e5fe190671417a16aef41060cc67c6172bd988f3
MrNovan <toni.justdoit@bk.ru>
2023-06-17 19:13:07 +0300

    add new avatars

--- src/avatars.js
+++ src/avatars.js
@@ -2,7 +2,12 @@ import readlineSync from 'readline-sync';
 import chalk from 'chalk';
 
 const avatarCat = [chalk.white('  (\\__/)\n  (=‘.’=)\nE[:]|||||[:]З\n  (“)_(”)'),
-  (chalk.hex('#FF8923')("`(“).(”)\n`(’ o ') \n (“) `(”)\n(“”)-(“”)"))];
+  (chalk.cyanBright('/\\_/\\\n((@v@))\n():::()\n VV-VV')),
+  chalk.red('/\\_/\\\n( o.o )\n > ^ <'),
+  chalk.green('((...))\n ( o o )\n  \\   /\n   ^_^'),
+  chalk.yellowBright('_   _\n(_)_(_)\n (o o)\n==\\o/=='),
+  chalk.blue('((_,...,_))\n    |o o|\n    \\   /\n     ^_^'),
+  chalk.magentaBright('__    __\n/ \\\\..// \\\n  ( oo )\n   \\__/')];
 
 const selectAvatar = () => {
   const indexAvatar = readlineSync.keyInSelect(avatarCat, chalk.hex('#EFC09D')('Выбери аватарку:'));
