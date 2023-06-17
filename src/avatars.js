126e84c82420fbfff699096a7b1c561c3a31b4d0
MrNovan <toni.justdoit@bk.ru>
2023-06-17 19:31:53 +0300

    fix new avatars

--- src/avatars.js
+++ src/avatars.js
@@ -1,13 +1,13 @@
 import readlineSync from 'readline-sync';
 import chalk from 'chalk';
 
-const avatarCat = [chalk.white('  (\\__/)\n  (=‘.’=)\nE[:]|||||[:]З\n  (“)_(”)'),
-  (chalk.cyanBright('/\\_/\\\n((@v@))\n():::()\n VV-VV')),
-  chalk.red('/\\_/\\\n( o.o )\n > ^ <'),
-  chalk.green('((...))\n ( o o )\n  \\   /\n   ^_^'),
-  chalk.yellowBright('_   _\n(_)_(_)\n (o o)\n==\\o/=='),
-  chalk.blue('((_,...,_))\n    |o o|\n    \\   /\n     ^_^'),
-  chalk.magentaBright('__    __\n/ \\\\..// \\\n  ( oo )\n   \\__/')];
+const avatarCat = [chalk.white.bold('  (\\__/)\n  (=‘.’=)\nE[:]|||||[:]З\n  (“)_(”)'),
+  (chalk.cyanBright.bold('/\\_/\\\n((@v@))\n():::()\n VV-VV')),
+  chalk.red.bold('/\\_/\\\n( o.o )\n > ^ <'),
+  chalk.green.bold('((...))\n ( o o )\n  \\   /\n   ^_^'),
+  chalk.yellowBright.bold('_   _\n(_)_(_)\n (o o)\n==\\o/=='),
+  chalk.blue.bold('((_,...,_))\n    |o o|\n    \\   /\n     ^_^'),
+  chalk.magentaBright.bold('__    __\n/ \\\\..// \\\n  ( oo )\n   \\__/')];
 
 const selectAvatar = () => {
   const indexAvatar = readlineSync.keyInSelect(avatarCat, chalk.hex('#EFC09D')('Выбери аватарку:'));
