8fb5abd917505ebbb4b217c5214949f40dbed45f
MrNovan <toni.justdoit@bk.ru>
2023-06-19 19:24:56 +0300

    fix new avatars

--- src/avatars.js
+++ src/avatars.js
@@ -2,7 +2,7 @@ import readlineSync from 'readline-sync';
 import chalk from 'chalk';
 
 const avatarCat = [chalk.white.bold('  (\\__/)\n  (=‘.’=)\nE[:]|||||[:]З\n  (“)_(”)'),
-  (chalk.cyanBright.bold('/\\_/\\\n((@v@))\n():::()\n VV-VV')),
+  chalk.cyanBright.bold('/\\_/\\\n((@v@))\n():::()\n VV-VV'),
   chalk.red.bold('/\\_/\\\n( o.o )\n > ^ <'),
   chalk.green.bold('((...))\n ( o o )\n  \\   /\n   ^_^'),
   chalk.yellowBright.bold('_   _\n(_)_(_)\n (o o)\n==\\o/=='),
